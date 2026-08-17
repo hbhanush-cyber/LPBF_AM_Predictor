972970c1c5f05db0f0a85d8456bb8c3e0d29ebb7
hbhanush-cyber <hbhanush@umd.edu>
2026-08-16 22:42:20 -0400

    Update UNetTest3D.py

--- models/UNetTest3D.py
+++ models/UNetTest3D.py
@@ -130,8 +130,8 @@ training_files = [
 
 testingDataFile = DATA_DIR / "layers525-650CYLINDER10Updated.pt"
 
-RESUME_CHECKPOINT = None
-RESUME_EPOCH = 0
+RESUME_CHECKPOINT = DATA_DIR / "checkpoint_epoch100AdamWABatchNorm"
+RESUME_EPOCH = 100
 
 # ==========================================================
 # INPUT CHANNEL SELECTION
